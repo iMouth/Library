41b51bb9ae276c89c908e2c2c822687d2f013388
Kelvin Etori <kelvin@email.arizona.edu>
2021-07-29 22:29:18 -0700

    starting webpage

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+myLibrary.add;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return title + " " + author + " " + pages + " " + read;
+  };
+}
+
+function addBookToLibrary() {}
